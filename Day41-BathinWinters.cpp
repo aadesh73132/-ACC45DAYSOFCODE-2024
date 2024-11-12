d634c77d4d093f8e840201fbb7c96cbd9732100f
aadesh73132 <145355002+aadesh73132@users.noreply.github.com>
2024-11-12 20:01:47 +0530

    Day 41 - Bath in Winters.

---
+++ Day41-BathinWinters.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t, x, y;
+    cin >> t;
+    while (t--)
+    {
+        cin >> x >> y;
+
+        if ((y * 2) <= x)
+            cout << (x / (2 * y)) << endl;
+
+        else
+            cout << "0" << endl;
+    }
+    return 0;
+
+    return 0;
+}
